d4bb66295ae125d6194627433c867b16acacc9ee
lrusli <rusli.lawrence@gmail.com>
2015-08-13 18:15:14 -0700

    Finish chapter 8 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,18 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   # Class method User.digest
-  # Returns the hex digest of a given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  class << self
+    # Returns the hex digest of a given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  # Returns a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # Remembers a user in the database for persistent sessions.
