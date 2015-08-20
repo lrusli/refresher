2febf266d472c28c274c829487efa340d0be623d
lrusli <rusli.lawrence@gmail.com>
2015-08-19 19:40:33 -0700

    Fix SendGrid config typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,14 @@ Rails.application.configure do
   host = 'serene-inlet-4645.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :addresses => 'smtp.sendgrid.net',
-    :port      => '587',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password  => ENV['SENDGRID_PASSWORD'],
-    :domain    => 'heroku.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
-  }
+  } 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
